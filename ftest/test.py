37aa8b29e1ced8b5e5d5f8db7c498367381f15b1
S0M0-G <275528307+S0M0-G@users.noreply.github.com>
2026-07-20 10:06:31 +0100

    restructuration du contenu de ftest

--- ftest/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-user=input("entrer votre prenom:")
-a=int(input("entrer un chiffre quelconque:"))
-b=int(input("entrer encore un chiffre de votre choix:"))
-if (a+b)==15:
-	print("hello" ,user,"vous avez été super")
-else:
-	print("hello",user,"malheureusement c'est pas le bon resulat")
-
